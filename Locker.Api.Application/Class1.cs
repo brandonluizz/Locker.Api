6bb868179d5c3ded63c6c8f9a1c3e1487fcb111c
Unknown <brandonluizz@hotmail.com>
2017-05-24 00:38:35 -0300

    security commit

--- Locker.Api.Application/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Locker.Api.Application
-{
-    public class Class1
-    {
-    }
-}
--- Locker.Api.DomainModel/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Locker.Api.DomainModel
-{
-    public class Class1
-    {
-    }
-}
